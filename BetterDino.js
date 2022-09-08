eba88938efba55ac8de8629ab8bf8b15ee265853
iinsertNameHere <83466692+iinsertNameHere@users.noreply.github.com>
2022-09-08 17:38:34 +0200

    Update BetterDino.js

--- BetterDino.js
+++ BetterDino.js
@@ -4,8 +4,7 @@ window.addEventListener("keydown", checkKeyPressed, false); function checkKeyPre
 Runner.prototype.oGameOver = Runner.prototype.gameOver;
 Runner.instance_.oSpeed = Runner.instance_.currentSpeed;
 
-$('body').append(
-"<script id='betterdino_def'>" +
+$('body').append("<script id='betterdino_def'>" +
 "function drawlaser() {" +
 "if (Runner.instance_.horizon.obstacles.length>0){" +
 "Runner.instance_.clearCanvas=function(){};" +
@@ -16,47 +15,42 @@ $('body').append(
 "setTimeout(function(){Runner.instance_.clearCanvas = b;}, 35);" +
 "Runner.instance_.horizon.obstacles = [];" +
 "}}" +
-"</script>"
-);
+"</script>");
 
-$('body').append(
-"<script id='betterdino_functions'>" +
+$('body').append("<script id='betterdino_functions'>" +
 "function godmodeon(){" +
 "Runner.prototype.gameOver = function(){};" +
-"document.getElementById('gm_s').innerText = 'On';" +
+"document.getElementById('gms').innerText = 'On';" +
 "console.log('GodMode: On');" +
 "}" +
 "function godmodeoff(){" +
 "Runner.prototype.gameOver = Runner.prototype.oGameOver;" +
-"document.getElementById('gm_s').innerText = 'Off';" +
+"document.getElementById('gms').innerText = 'Off';" +
 "console.log('GodMode: Off');" +
 "}" +
 "function setspeed(){" +
 "var speed = parseInt(prompt('Enter Speed Value:', '0'));" +
 "Runner.instance_.setSpeed(speed);" +
-"document.getElementById('sp_s').innerText = speed" +
+"document.getElementById('sps').innerText = speed" +
 "console.log('Changed Speed');" +
 "}" +
 "function resetspeed(){" +
 "Runner.instance_.setSpeed(Runner.instance_.oSpeed);" +
-"document.getElementById('sp_s').innerText = 'Normal'" +
+"document.getElementById('sps').innerText = 'Normal'" +
 "console.log('SetSpeed: Normal');" +
 "}" +
-"</script>"
-);
+"</script>");
 
 $('body').append("<style id='betterdino_style'>#top {position: fixed;top: 0;left: 0;z-index: 999;width: 100%;height: 23px;}</style>")
 
-$('html').append(
-"<div id='top'>" +
+$('html').append("<div id='top'>" +
 "<button onclick='godmodeon()'>GodMode On</button>" +
 "<button onclick='godmodeoff()'>GodMode Off</button>" +
 "<button onclick='setspeed()'>Set Speed</button>" +
 "<button onclick='resetspeed()'>Reset Speed</button>" +
 "<p>Press D to shoot lasers!</p>" +
-"<p>GodMode: <p id='gm_s' style='color: blue'>Off</p></p>" +
-"<p>Speed: <p id='sp_s' style='color: blue'>Normal</p></p>" +
-"</div>"
-);
+"<p>GodMode: <p id='gms' style='color: blue'>Off</p></p>" +
+"<p>Speed: <p id='sps' style='color: blue'>Normal</p></p>" +
+"</div>");
 
 alert('BetterDino Loaded!');
