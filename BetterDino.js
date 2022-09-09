e044a301f8ed5a9b3f0d1f46fc0886e6630e982a
iinsertNameHere <iinsert.mailhere@gmail.com>
2022-09-09 14:26:56 +0200

    Remove Backcolor

--- BetterDino.js
+++ BetterDino.js
@@ -58,28 +58,28 @@ $('body').append(
 "}" +
 "function lightskin(){" +
 "Runner.imageSprite.src = 'https://chromedino.com/assets/offline-sprite-1x.png'" +
-"document.body.style.backgroundColor = '#f7f7f7';" +
-"document.getElementById('d_info').style.color = '#333';" +
+//"document.body.style.backgroundColor = '#f7f7f7';" +
+//"document.getElementById('d_info').style.color = '#333';" +
 "}" +
 "function darkskin(){" +
 "Runner.imageSprite.src = 'https://chromedino.com/assets/offline-sprite-1x-black.png'" +
-"document.body.style.backgroundColor = '#000';" +
-"document.getElementById('d_info').style.color = 'white';" +
+//"document.body.style.backgroundColor = '#000';" +
+//"document.getElementById('d_info').style.color = 'white';" +
 "}" +
 "function coloredskin(){" +
 "Runner.imageSprite.src = 'https://chromedino.com/assets/chromedino_coloured-1x.png'" +
-"document.body.style.backgroundColor = '#f7f7f7';" +
-"document.getElementById('d_info').style.color = '#333';" +
+//"document.body.style.backgroundColor = '#f7f7f7';" +
+//"document.getElementById('d_info').style.color = '#333';" +
 "}" +
 "function marioskin(){" +
 "Runner.imageSprite.src = 'https://chromedino.com/assets/offline-sprite-1x-mario.png'" +
-"document.body.style.backgroundColor = '#75a5f9';" +
-"document.getElementById('d_info').style.color = '#333';" +
+//"document.body.style.backgroundColor = '#75a5f9';" +
+//"document.getElementById('d_info').style.color = '#333';" +
 "}" +
 "function batmanskin(){" +
 "Runner.imageSprite.src = 'https://chromedino.com/assets/batman1x.png';" +
-"document.body.style.backgroundColor = '#000';" +
-"document.getElementById('d_info').style.color = 'white';" +
+//"document.body.style.backgroundColor = '#000';" +
+//"document.getElementById('d_info').style.color = 'white';" +
 "}" +
 "</script>");
 
